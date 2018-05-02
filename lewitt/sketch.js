cbccb92f064a54b94b130a7bd436d9fee134fd41
pfe1223 <patrickester@APP-1554.local>
2018-05-02 16:35:23 -0700

    Add JS sketch
    Add a P5 sketch at the bottom of the site.

--- lewitt/sketch.js
+++ lewitt/sketch.js
@@ -1,7 +1,20 @@
+let c; //canvas variable
+let x = -20;
+
 function setup() {
-  createCanvas(400, 400);
+  c = createCanvas(400, 400);
+  c.parent('js');
 }
 
 function draw() {
+  background('#ecf0f1');
+  fill(0);
+  shape1();
+}
 
+function shape1() {
+  for (let i = 0; i < 200; i += 10) {
+    stroke(0);
+    line(i, 0, 0, i);
+  }
 }
