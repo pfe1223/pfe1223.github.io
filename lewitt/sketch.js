fd18068f6147f71e6eb7d8c07749001d658bcb56
pfe1223 <patrickester@APP-1554.local>
2018-05-02 16:57:44 -0700

    Add random forms
    Add code to click the mouse on the canvas to randomly select a form to
    be displayed.

--- lewitt/sketch.js
+++ lewitt/sketch.js
@@ -1,20 +1,72 @@
-let c; //canvas variable
-let x = -20;
+let myCanvas; //canvas variable
+let count = 2; //number of functions
 
 function setup() {
-  c = createCanvas(400, 400);
-  c.parent('js');
-}
-
-function draw() {
+  myCanvas = createCanvas(400, 400);
+  myCanvas.parent('js');
   background('#ecf0f1');
-  fill(0);
-  shape1();
+  strokeWeight(3);
 }
 
-function shape1() {
-  for (let i = 0; i < 200; i += 10) {
+// function draw() {
+//   background('#ecf0f1');
+//   shape1();
+// }
+
+function mousePressed() {
+  if (mouseX > 0 && mouseX <= width) {
+    let choice = floor(random(count + 1));
+    if (choice === 1) {
+      form1();
+    } else if (choice === 2) {
+      form2();
+    }
+  }
+}
+
+function form1() {
+  background('#ecf0f1');
+  for (let i = 0; i <= 200; i += 10) {
     stroke(0);
     line(i, 0, 0, i);
+  }
+
+  for (let i = 1; i <= 400; i += 50) {
+    noFill();
+    ellipse(i + 20, 200, 40, 40);
+  }
+
+  for (let i = 0; i <= 400; i += 20) {
+    line(i, height - i, i, height - i + 40);
+  }
+}
+
+function form2() {
+  background('#ecf0f1');
+  rectMode(CENTER);
+  for (let i = 0; i < width; i += 15) {
+    noFill();
+    rect(i, i, 20, 20);
+  }
+
+  for (let i = 0; i < height; i += 40) {
+    line(width - i, height - i, i, height - i);
+  }
+
+  for (let i = 0; i < 5; i++) {
+    let x = random(width);
+    let y = random(height);
+    let s = random(20, 90);
+    fill(0);
+    ellipse(x, y, s);
+  }
+
+  for (let i = 0; i < 50; i++) {
+    if (i % 2 === 0) {
+      fill(0);
+    } else {
+      noFill();
+    }
+    rect(i * 10, height - i * 10, 5 * i, 10);
   }
 }
