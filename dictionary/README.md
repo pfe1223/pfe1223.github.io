c2ab4321a4e6b5263e5683c628332346b97e0be6
pfe1223 <patrick.ester@gmail.com>
2018-10-30 16:17:15 -0700

    Math Dictionary
    Add the math dictionary (what is working so far) to the site.

---
+++ dictionary/README.md
@@ -0,0 +1,2 @@
+# mathDictionary
+Dictionary of Math terms in French and English. An image accompanies the definitions.
---
+++ dictionary/handleLogin.js
@@ -0,0 +1,33 @@
+function initClient() {
+  var API_KEY = 'AIzaSyAkupQZKQiunQz30q47wufjUw_H6O_aJW8';  
+  var CLIENT_ID = '222486791717-tgujfgiet2epa5i7emc2u0oi14v0koup.apps.googleusercontent.com ';
+  var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+
+  gapi.client.init({
+    'apiKey': API_KEY,
+    'clientId': CLIENT_ID,
+    'scope': SCOPE,
+    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  }).then(function() {
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+  });
+}
+
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+function updateSignInStatus(isSignedIn) {
+  if (isSignedIn) {
+    read_data();
+  }
+}
+
+function handleSignInClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignOutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
